51159f9c3f30cec681c1cceb63d962802d75f853
ZiyadZM <ZiyadZabin@gmail>
2023-09-04 18:07:56 +0300

    first

---
+++ de.py
@@ -0,0 +1,22 @@
+def xor_encrypt(text):
+    encrypted = bytearray()
+    for i in range(len(text)):
+        encrypted.append(text[i] ^ 1)
+    return encrypted
+def xor_decrypt(encrypted):
+    decrypted = bytearray()
+    for i in range(len(encrypted)):
+        decrypted.append(encrypted[i] ^ 1)
+    return decrypted
+
+plaintext = "Ziyad"
+
+encrypted_data = xor_encrypt(plaintext.encode())
+hex_encrypted = encrypted_data.hex()
+print("Encrypted: ",encrypted_data )
+print("Hexadecimal:", hex_encrypted)
+
+decrypted_data = xor_decrypt(encrypted_data)
+hex_decrypted = decrypted_data.hex()
+print("Decrypted: ",decrypted_data.decode())
+print("Hexadecimal:", hex_decrypted)
